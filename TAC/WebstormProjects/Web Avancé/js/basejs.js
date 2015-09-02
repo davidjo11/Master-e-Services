5b5128be0d6e9e3cf183bd923c67f0cebdaf8ebe
David <dav.josias@gmail.com>
2015-09-02 22:12:33 +0200

    Exo Cours 1

---
+++ TAC/WebstormProjects/Web Avancé/js/basejs.js
@@ -0,0 +1,6 @@
+/**
+ * Created by David on 02/09/2015.
+ */
+
+// b.addEventListener( "event", obj, false)
+//addEventLsitener va aller chercher dans l'objet obj une méthode handleEvent pour l'éxécuter
