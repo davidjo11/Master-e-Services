04304d5b2e15c2a5ce27e6a246728b230d96d6f8
David <david.josias@etudiant.univ-lille1.fr>
2015-09-04 15:30:02 +0200

    Live Class 2 on bruh!

--- TAC/WebstormProjects/Web Avancé/DemoLive/LiveClass.js
+++ TAC/WebstormProjects/Web Avancé/DemoLive/LiveClass.js
@@ -5,3 +5,13 @@
 alert("Salut");
 
 //javascript:{baliseScript = document.createElement("script");baliseScript.src = "http://localhost:63342/WebstormProjects/Web Avancé/DemoLive/LiveClass.js";document.getElementsByTagName("body")[0].appendChild(baliseScript);}
+bg = document.createElement("div");
+bg.style.backgroundColor = "rgba(0,0,0,0.5)";
+bg.style.position = "absolute";
+bg.style.top = "0";
+bg.style.left = "0";
+bg.style.height = "50%";
+bg.style.width = "100%";
+bg.style.zIndex = "300";
+
+document.getElementsByTagName("body")[0].appendChild(bg);
