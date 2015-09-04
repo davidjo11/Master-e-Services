f65c58f859572fd04f0f5d2e0d8c2b209b67dc76
David <david.josias@etudiant.univ-lille1.fr>
2015-09-04 16:31:58 +0200

    Live Class 3 on bruh!

--- TAC/WebstormProjects/Web Avancé/DemoLive/LiveClass.js
+++ TAC/WebstormProjects/Web Avancé/DemoLive/LiveClass.js
@@ -2,16 +2,53 @@
  * Created by josias on 04/09/15.
  */
 
-alert("Salut");
 
 //javascript:{baliseScript = document.createElement("script");baliseScript.src = "http://localhost:63342/WebstormProjects/Web Avancé/DemoLive/LiveClass.js";document.getElementsByTagName("body")[0].appendChild(baliseScript);}
-bg = document.createElement("div");
-bg.style.backgroundColor = "rgba(0,0,0,0.5)";
-bg.style.position = "absolute";
-bg.style.top = "0";
-bg.style.left = "0";
-bg.style.height = "50%";
-bg.style.width = "100%";
-bg.style.zIndex = "300";
+
+
+createStyleElement = function (){
+    var element = document.createElement(arguments[0]);
+    for(var i=1;i<arguments.length;i++){
+        element.style[arguments[i++]] = arguments[i];
+        console.log(i);
+    }
+    return element;
+};
+
+bg = createStyleElement("div",
+        "position", "absolute",
+        "backgroundColor", "black",
+        "opacity", "0.5",
+        "top", "0",
+        "left" , "0",
+        "height", "50%",
+        "width", "100%",
+        "zIndex", "300"
+    );
+
+bg2 = createStyleElement("div",
+        "margin", "auto",
+        "backgroundColor", "white",
+        "width", "50%",
+        "height", "50%",
+        "position", "relative"
+    );
+
+bg2.appendChild(document.createTextNode("Titre:"));
+
+button1 = createStyleElement("button",
+        "position", "absolute",
+        "height", "20px",
+        "width", "20px",
+        "bottom", "5%",
+        "right", "5%",
+        "border-radius", "50px"
+    );
+
+button1.html = "Ok";
+
+bg2.appendChild(button1);
+
+bg.appendChild(bg2);
 
 document.getElementsByTagName("body")[0].appendChild(bg);
