8fc74b60d3bee590bc5816c52c47e80d4b528b2e
David <david.josias@etudiant.univ-lille1.fr>
2015-09-04 14:59:34 +0200

    Live Class on bruh!

---
+++ TAC/WebstormProjects/Web Avancé/DemoLive/LiveClass.js
@@ -0,0 +1,7 @@
+/**
+ * Created by josias on 04/09/15.
+ */
+
+alert("Salut");
+
+//javascript:{baliseScript = document.createElement("script");baliseScript.src = "http://localhost:63342/WebstormProjects/Web Avancé/DemoLive/LiveClass.js";document.getElementsByTagName("body")[0].appendChild(baliseScript);}
